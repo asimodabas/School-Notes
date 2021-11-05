fb4ee4b1c7bb63b03cbf9a9f9492bad110ced516
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2021-11-06 00:39:44 +0300

    menu page and detail page added

---
+++ app/src/main/java/com/asimodabas/school_notes/DetayActivity.kt
@@ -0,0 +1,55 @@
+package com.asimodabas.school_notes
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_detay.*
+import kotlinx.android.synthetic.main.activity_main.*
+
+class DetayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detay)
+
+        toolbarNotDetay.title = "Note Detail"
+        setSupportActionBar(toolbarNotDetay)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.action_sil -> { Snackbar.make(toolbarNotDetay,"Are you want to delete this ?", Snackbar.LENGTH_SHORT)
+                    .setAction("YES"){
+
+                        startActivity(Intent(this@DetayActivity, MainActivity::class.java))
+                        finish()
+                    }.show()
+
+                return true
+            }
+
+            R.id.action_duzenle -> {
+
+                startActivity(Intent(this@DetayActivity, MainActivity::class.java))
+                finish()
+
+                return true
+            }
+            else ->
+                return false
+
+        }
+    }
+
+}
